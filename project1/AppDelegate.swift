fe8cb889c2e8a99637d786a2d854c21ad2f35484
Evil <macbookpro@nobody.invalid>
2017-08-30 19:12:28 +0430

    Initial Commit

---
+++ project1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  project1
+//
+//  Created by Evil on .
+//  Copyright © 2017 Evil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ project1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Evil on .
+//  Copyright © 2017 Evil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
